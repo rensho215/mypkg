3e364e93ed904d4c026affa6cbcb28c4eea84754
rensho215 <72724172+rensho215@users.noreply.github.com>
2021-01-08 19:30:07 +0900

    Update and rename twice.py to two-three.py

--- scripts/two-three.py
+++ scripts/two-three.py
@@ -4,16 +4,19 @@ import rospy
 from std_msgs.msg import Int32
 
 n = 0
+m = 0
 
 def cb(message):
-    global n
+    global n,m
     n = message.data*2
+    m = message.data*3
 
 if __name__ == '__main__':
-    rospy.init_node('twice')
+    rospy.init_node('two-three')
     sub = rospy.Subscriber('count_up', Int32, cb)
-    pub = rospy.Publisher('twice', Int32, queue_size=1)
+    pub = rospy.Publisher('two-three', Int32, queue_size=1)
     rate = rospy.Rate(10)
     while not rospy.is_shutdown():
         pub.publish(n)
+        pub.publish(m)
         rate.sleep()
